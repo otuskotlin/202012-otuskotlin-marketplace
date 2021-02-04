9f222dac9a547145a807aec7d66d15877ad47f64
Sergey Okatov <sokatov@gmail.com>
2021-02-04 12:02:34 +0500

    M2L2-s0 инициализация общего модуля

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,12 @@
 rootProject.name = "otuskotlin-marketplace"
 
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("multiplatform") version kotlinVersion
+        kotlin("jvm") version kotlinVersion
+    }
+}
+
+include("ok-marketplace-common-be")
+include("ok-marketplace-common-mp")
