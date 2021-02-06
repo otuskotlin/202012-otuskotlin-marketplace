b3390cdf555468ed32ebf86b9aace05c8d1ac34b
Sergey Okatov <sokatov@gmail.com>
2021-02-06 11:25:59 +0500

    M2L2 s4 Рефакторинг кода с учетом тестов

--- ok-marketplace-common-mp/src/commonTest/kotlin/ru/otus/otuskotlin/marketrplace/common/kmp/validation/ValidationTest.kt
+++ ok-marketplace-common-mp/src/commonTest/kotlin/ru/otus/otuskotlin/marketrplace/common/kmp/validation/ValidationTest.kt
@@ -1,5 +1,7 @@
 package ru.otus.otuskotlin.marketrplace.common.kmp.validation
 
+import ru.otus.otuskotlin.marketplace.common.kmp.validation.validators.ValidatorIntInRange
+import ru.otus.otuskotlin.marketplace.common.kmp.validation.validators.ValidatorStringNonEmpty
 import runBlockingTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -9,7 +11,7 @@ internal class ValidationTest {
     @Test
     fun createValidationTest() = runBlockingTest {
         val validator = ValidatorStringNonEmpty()
-        val res = validator.validate("")
+        val res = validator validate ""
         assertEquals(false, res.isSuccess)
         assertTrue {
             res.errors.first().message.contains("empty")
@@ -27,65 +29,3 @@ internal class ValidationTest {
     }
 }
 
-class ValidatorIntInRange<T : Comparable<T>>(
-    private val field: String,
-    private val min: T,
-    private val max: T
-) {
-    infix fun validate(sample: T): ValidationResult = if (sample in min..max) {
-        ValidationResult.SUCCESS
-    } else {
-        ValidationResult(
-            errors = listOf(
-                ValidationFieldError(
-                    message = "Value $sample for field $field exceeds range [$min, $max]",
-                    field = field
-                )
-            )
-        )
-    }
-}
-
-class ValidatorStringNonEmpty {
-
-    fun validate(sample: String?): ValidationResult {
-        return if (sample.isNullOrBlank()) {
-            ValidationResult(
-                errors = listOf(
-                    ValidationDefaultError(
-                        message = "String must not be empty",
-                    )
-                )
-            )
-        } else {
-            ValidationResult.SUCCESS
-        }
-    }
-
-}
-
-class ValidationResult(val errors: List<IValidationError>) {
-    val isSuccess: Boolean
-        get() = errors.isEmpty()
-
-    companion object {
-        val SUCCESS = ValidationResult(emptyList())
-    }
-}
-
-data class ValidationDefaultError(
-    override val message: String,
-) : IValidationError
-
-data class ValidationFieldError(
-    override val message: String,
-    override val field: String,
-) : IValidationError, IValidationFieldError
-
-interface IValidationError {
-    val message: String
-}
-
-interface IValidationFieldError : IValidationError {
-    val field: String
-}
