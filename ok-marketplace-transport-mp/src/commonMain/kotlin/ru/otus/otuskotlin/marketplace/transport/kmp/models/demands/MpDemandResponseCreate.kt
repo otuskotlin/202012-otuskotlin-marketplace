644c2ec5e59c8f98876ff7d41ea81af2b8ea702f
DTadescu <tada.mascot@gmail.com>
2021-02-08 00:52:57 +0500

    M3L1-s3 CRUD для Proposals

--- ok-marketplace-transport-mp/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/transport/kmp/models/demands/MpDemandResponseCreate.kt
+++ ok-marketplace-transport-mp/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/transport/kmp/models/demands/MpDemandResponseCreate.kt
@@ -8,13 +8,13 @@ import ru.otus.otuskotlin.marketplace.transport.kmp.models.common.IMpResponse
 import ru.otus.otuskotlin.marketplace.transport.kmp.models.common.ResponseStatusDto
 
 @Serializable
-@SerialName("MpDemandResponse")
-data class MpDemandResponse(
+@SerialName("MpDemandResponseCreate")
+data class MpDemandResponseCreate(
     override val id: String? = null,
     override val onRequest: String? = null,
     override val endTime: String? = null,
     override val debug: DebugDto? = null,
     override val errors: List<ErrorDto>? = null,
     override val status: ResponseStatusDto? = null,
-    val responseDemand: DemandDto? = null,
-    ): IMpResponse
+    val demand: DemandDto? = null,
+): IMpResponse
---
+++ ok-marketplace-transport-mp/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/transport/kmp/models/proposals/MpProposalResponseRead.kt
@@ -0,0 +1,20 @@
+package ru.otus.otuskotlin.marketplace.transport.kmp.models.proposals
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import ru.otus.otuskotlin.marketplace.transport.kmp.models.common.ErrorDto
+import ru.otus.otuskotlin.marketplace.transport.kmp.models.common.DebugDto
+import ru.otus.otuskotlin.marketplace.transport.kmp.models.common.IMpResponse
+import ru.otus.otuskotlin.marketplace.transport.kmp.models.common.ResponseStatusDto
+
+@Serializable
+@SerialName("MpProposalResponse")
+data class MpProposalResponseRead(
+    override val id: String? = null,
+    override val onRequest: String? = null,
+    override val endTime: String? = null,
+    override val debug: DebugDto? = null,
+    override val errors: List<ErrorDto>? = null,
+    override val status: ResponseStatusDto? = null,
+    val proposal: ProposalDto? = null,
+    ): IMpResponse
