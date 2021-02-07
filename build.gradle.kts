8c2d643e19b52fbc729d88ae3271e22c2cda773d
Sergey Okatov <sokatov@gmail.com>
2021-02-07 23:03:08 +0500

    M3L2 s4 генерация моделей, исправление ошибок

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     kotlin("multiplatform") apply false
     kotlin("jvm") apply false
     kotlin("js") apply false
+
+    id("org.openapi.generator") apply false
 }
 
 group = "ru.otus.otuskotlin.marketplace"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,12 +2,20 @@ rootProject.name = "otuskotlin-marketplace"
 
 pluginManagement {
     val kotlinVersion: String by settings
+    val openapiVersion: String by settings
+
     plugins {
         kotlin("multiplatform") version kotlinVersion
         kotlin("jvm") version kotlinVersion
         kotlin("js") version kotlinVersion
+
+        id("org.openapi.generator") version openapiVersion
     }
 }
 
 include("ok-marketplace-common-mp")
 include("ok-marketplace-common-be")
+include("ok-marketplace-transport-openapi-demand-kt")
+include("ok-marketplace-transport-openapi-demandoffers-kt")
+include("ok-marketplace-transport-openapi-proposal-kt")
+include("ok-marketplace-transport-openapi-proposaloffers-kt")
