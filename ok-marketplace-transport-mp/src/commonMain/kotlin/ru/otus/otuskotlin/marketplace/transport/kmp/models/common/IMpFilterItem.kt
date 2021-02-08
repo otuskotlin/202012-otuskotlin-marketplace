d094263b8cc3115afde5ff4b9b5563c4a5ce123f
DTadescu <tada.mascot@gmail.com>
2021-02-08 23:13:36 +0500

    M3L1-s3 Названия моделей поменяны

--- ok-marketplace-transport-mp/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/transport/kmp/models/common/IMpFilterItem.kt
+++
@@ -1,5 +0,0 @@
-package ru.otus.otuskotlin.marketplace.transport.kmp.models.common
-
-interface IMpFilterItem {
-    val text: String?
-}
--- ok-marketplace-transport-mp/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/transport/kmp/models/proposals/MpProposalListFilterDto.kt
+++
@@ -1,9 +0,0 @@
-package ru.otus.otuskotlin.marketplace.transport.kmp.models.proposals
-
-import kotlinx.serialization.Serializable
-import ru.otus.otuskotlin.marketplace.transport.kmp.models.common.IMpFilterItem
-
-@Serializable
-data class MpProposalListFilterDto(
-    override val text: String? = null,
-): IMpFilterItem
