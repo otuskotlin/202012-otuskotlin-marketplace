6f2099a06b98ef3abc07020a04667f0de9617478
Ivan Babintsev <ibabintsev@mfms.ru>
2021-03-17 13:49:10 +0300

    Added stub cases to context

--- ok-marketplace-be-common/src/main/kotlin/ru/otus/otuskotlin/marketplace/common/backend/context/MpBeContext.kt
+++ ok-marketplace-be-common/src/main/kotlin/ru/otus/otuskotlin/marketplace/common/backend/context/MpBeContext.kt
@@ -5,6 +5,7 @@ import ru.otus.otuskotlin.marketplace.common.backend.models.*
 data class MpBeContext(
     var status: MpBeContextStatus = MpBeContextStatus.NONE,
     var errors: MutableList<IMpError> = mutableListOf(),
+    var stubCase: MpStubCase = MpStubCase.NONE,
 
     var requestDemandId: MpDemandIdModel = MpDemandIdModel.NONE,
     var requestProposalId: MpProposalIdModel = MpProposalIdModel.NONE,
