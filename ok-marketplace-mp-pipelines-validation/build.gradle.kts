f3bf9a9c78db3a87f416306187addfea4789eabb
Sergey Okatov <sokatov@gmail.com>
2021-03-20 09:18:08 +0500

    M5L3 s1 инициализация модуля

---
+++ ok-marketplace-mp-pipelines-validation/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+group = rootProject.group
+version = rootProject.version
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    js {
+        browser { }
+        nodejs { }
+    }
+    jvm { }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-js"))
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+    }
+}
