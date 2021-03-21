e6b96c36754bd5d441109a21f88abead3caeb76d
Sergey Okatov <sokatov@gmail.com>
2021-03-21 13:46:33 +0500

    M5l3 S7 Доделка эндпоинтов Demand

--- ok-marketplace-be-business-logic/src/main/kotlin/ru/otus/otuskotlin/marketplace/business/logic/backend/pipelines/DemandOffers.kt
+++ ok-marketplace-be-business-logic/src/main/kotlin/ru/otus/otuskotlin/marketplace/business/logic/backend/pipelines/DemandOffers.kt
@@ -1,9 +1,11 @@
 package ru.otus.otuskotlin.marketplace.business.logic.backend.pipelines
 
+import ru.otus.otuskotlin.marketplace.business.logic.backend.helpers.validation
 import ru.otus.otuskotlin.marketplace.business.logic.backend.operations.CompletePipeline
 import ru.otus.otuskotlin.marketplace.business.logic.backend.operations.InitializePipeline
 import ru.otus.otuskotlin.marketplace.business.logic.backend.operations.stubs.DemandOffersStub
 import ru.otus.otuskotlin.marketplace.common.backend.context.MpBeContext
+import ru.otus.otuskotlin.marketplace.common.kmp.validation.validators.ValidatorStringNonEmpty
 import ru.otus.otuskotlin.marketplace.pipelines.kmp.IOperation
 import ru.otus.otuskotlin.marketplace.pipelines.kmp.pipeline
 
@@ -11,6 +13,18 @@ object DemandOffers : IOperation<MpBeContext> by pipeline({
     execute(InitializePipeline)
 
     execute(DemandOffersStub)
+
+    validation {
+        validate<String?> {
+            on { requestDemandId.id }
+            validator(
+                ValidatorStringNonEmpty(
+                field = "demand-id",
+                message = "Demand ID requested must not be empty"
+            )
+            )
+        }
+    }
 
     execute(CompletePipeline)
 })
